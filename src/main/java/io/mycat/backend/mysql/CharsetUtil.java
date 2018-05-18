dd3ce3eb387e8775005f77651b28a5bb16695807
newskyddm <wangxinyu@huawei.com>
2018-05-18 09:11:44 +0800

    支持information_schema.columns

--- src/main/java/io/mycat/backend/mysql/CharsetUtil.java
+++ src/main/java/io/mycat/backend/mysql/CharsetUtil.java
@@ -27,6 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -37,6 +38,7 @@ public class CharsetUtil {
             .getLogger(CharsetUtil.class);
     private static final Map<Integer,String> INDEX_TO_CHARSET = new HashMap<>();
     private static final Map<String, Integer> CHARSET_TO_INDEX = new HashMap<>();
+    private static final Map<String, String> CHARSET_TO_JAVA = new HashMap<>();
     static {
 
         // index_to_charset.properties
@@ -73,6 +75,54 @@ public class CharsetUtil {
         CHARSET_TO_INDEX.put("iso-8859-1", 14);
         CHARSET_TO_INDEX.put("iso_8859_1", 14);
         CHARSET_TO_INDEX.put("utf-8", 33);
+
+
+        CHARSET_TO_JAVA.put("usa7", "US-ASCII");
+        CHARSET_TO_JAVA.put("ascii", "US-ASCII");
+        CHARSET_TO_JAVA.put("big5", "Big5");
+        CHARSET_TO_JAVA.put("dec8", "ISO8859_1");
+        CHARSET_TO_JAVA.put("cp850", "Cp850");
+        CHARSET_TO_JAVA.put("hp8", "ISO8859_1");
+        CHARSET_TO_JAVA.put("koi8r", "KOI8_R");
+        CHARSET_TO_JAVA.put("latin1", "Cp1252");
+        CHARSET_TO_JAVA.put("latin1_de", "ISO8859_1");
+        CHARSET_TO_JAVA.put("german1", "ISO8859_1");
+        CHARSET_TO_JAVA.put("danish", "ISO8859_1");
+        CHARSET_TO_JAVA.put("latin2", "ISO8859_2");
+        CHARSET_TO_JAVA.put("swe7", "ISO8859_1");
+        CHARSET_TO_JAVA.put("ascii", "US-ASCII");
+        CHARSET_TO_JAVA.put("ujis", "EUC_JP");
+        CHARSET_TO_JAVA.put("sjis", "SJIS");
+        CHARSET_TO_JAVA.put("hebrew", "ISO8859_8");
+        CHARSET_TO_JAVA.put("tis620", "TIS620");
+        CHARSET_TO_JAVA.put("euckr", "EUC_KR");
+        CHARSET_TO_JAVA.put("koi8u", "KOI8_U");
+        CHARSET_TO_JAVA.put("gb2312", "EUC_CN");
+        CHARSET_TO_JAVA.put("greek", "ISO8859_7");
+        CHARSET_TO_JAVA.put("cp1250", "Cp1250");
+        CHARSET_TO_JAVA.put("gbk", "GBK");
+        CHARSET_TO_JAVA.put("latin5", "ISO8859_9");
+        CHARSET_TO_JAVA.put("armscii8", "ISO8859_1");
+        CHARSET_TO_JAVA.put("utf8", "UTF-8");
+        CHARSET_TO_JAVA.put("ucs2", "UnicodeBig");
+        CHARSET_TO_JAVA.put("cp866", "Cp866");
+        CHARSET_TO_JAVA.put("keybcs2", "Cp895");
+        CHARSET_TO_JAVA.put("macce", "MacCentralEurope");
+        CHARSET_TO_JAVA.put("macroman", "MacRoman");
+        CHARSET_TO_JAVA.put("cp852", "Cp852");
+        CHARSET_TO_JAVA.put("latin7", "ISO8859_13");
+        CHARSET_TO_JAVA.put("utf8mb4", "UTF-8");
+        CHARSET_TO_JAVA.put("cp1251", "Cp1251");
+        CHARSET_TO_JAVA.put("utf16", "UTF-16");
+        CHARSET_TO_JAVA.put("utf16le", "UTF-16LE");
+        CHARSET_TO_JAVA.put("cp1256", "Cp1256");
+        CHARSET_TO_JAVA.put("cp1257", "Cp1257");
+        CHARSET_TO_JAVA.put("utf32", "UTF-32");
+        CHARSET_TO_JAVA.put("binary", "ISO8859_1");
+        CHARSET_TO_JAVA.put("geostd8", "US-ASCII");
+        CHARSET_TO_JAVA.put("cp932", "MS932");
+        CHARSET_TO_JAVA.put("eucjpms", "EUC_JP_Solaris");
+        CHARSET_TO_JAVA.put("gb18030", "GB18030");
     }
 
     public static final String getCharset(int index) {
@@ -88,6 +138,14 @@ public class CharsetUtil {
         }
     }
 
-
+    public static String getJavaCharset(String charset)
+    {
+        if (charset == null || charset.length() == 0)
+            return StandardCharsets.UTF_8.toString();
+        String javaCharset = CHARSET_TO_JAVA.get(charset);
+        if (javaCharset == null)
+            return StandardCharsets.UTF_8.toString();
+        return javaCharset;
+    }
 
 }
